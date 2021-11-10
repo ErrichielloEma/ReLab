2929cdcd2352e99a1dce1fadefdfb5c704b3cc88
ErrichielloEma <errichiello.emanuele@iisgalvanimi.edu.it>
2021-11-10 12:12:58 +0000

    Relab2

---
+++ ReLab2/src/main.py
@@ -0,0 +1,75 @@
+# Import the Flask module that has been installed.
+from flask import Flask
+from flask import send_file
+from flask import jsonify
+from flask import request
+from flask_pymongo import PyMongo
+from flask_cors import CORS
+
+# Creating a new "app" by using the Flask constructor. Passes __name__ as a parameter.
+app = Flask(__name__)
+
+# Stringa di connessione al DB
+app.config["MONGO_URI"] = "mongodb+srv://user_spiegazioni:2xR9laRHWUaSUDhi@cluster0.7ftto.mongodb.net/relab?retryWrites=true&w=majority" #Importante qui va specificato il nome del DB
+
+# Annotation that allows the function to be hit at the specific URL.
+@app.route("/")
+
+# Generic Python functino that returns "Hello world!"
+def index():
+    return "Hello world!"
+
+# Annotation that allows the function to be hit at the specific URL (/books).
+@app.route("/books")
+# Generic Python function that returns books.json
+def books():
+    return send_file('/workspace/ReLab/ReLab2/src/books.json')
+
+# Questa route effettua una find() su tutto il DB (si limita ai primi 100 risultati)
+@app.route('/addresses', methods=['GET'])
+def get_all_addresses():
+    mil4326WKT = mongo.db.Mil4326WKT
+    output = []
+    for s in mil4326WKT.find().limit(100):
+        output.append(s['INDIRIZZO'])
+    return jsonify({'result': output})
+
+project = {
+    '_id': 0,
+    "INDIRIZZO": 1,
+    "WGS84_X": 1,
+    "WGS84_Y":1,
+    "CLASSE_ENE":1,
+    "EP_H_ND":1,
+    "FOGLIO":1,
+    "CI_VETTORE":1
+}
+
+@app.route('/ci_vettore/<foglio>', methods=['GET'])
+def get_vettore(foglio):
+    mil4326WKT = mongo.db.Mil4326WKT
+    query = {
+        "FOGLIO" : foglio
+    }
+
+    return jsonify(list(mil4326WKT.find(query, project)))#Nota che abbiamo eliminato la chiave result perchè i dati sono già formattati
+
+@app.route('/ci_vettore/sezione/<sezione>', methods=['GET'])
+def get_vettoreeie(sezione):
+    mil4326WKT = mongo.db.Mil4326WKT
+    query = {
+        "SEZ" : sezione
+    }
+
+    return jsonify(list(mil4326WKT.find(query, project)))
+
+mongo = PyMongo(app)
+
+# Per rispondere alle chiamate cross origin
+CORS(app)
+
+# Checks to see if the name of the package is the run as the main package.
+if __name__ == "__main__":
+    # Runs the Flask application only if the main.py file is being run.
+    app.run()
+    
---
+++ ReLabNew/src/app/app.component.ts
@@ -0,0 +1,102 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'ReLabNew';
+  center: any;
+  position: any;
+  label: string;
+  position2: any;
+  positionProcione1: any;
+  positionProcione2: any;
+  positionProcione3: any;
+  label2: string;
+  circleOptions: { fillColor: string; };
+  markerOptions: google.maps.MarkerOptions;
+  markerOptions1: google.maps.MarkerOptions;
+  markerOptions2: google.maps.MarkerOptions;
+  markerOptions3: google.maps.MarkerOptions;
+  vertices: google.maps.LatLngLiteral[];
+  verticesOptions: { fillColor: string; };
+  vertices2: google.maps.LatLngLiteral[];
+  verticesOptions2: { fillColor: string; };
+  constructor() {
+    this.center = { lat: 45.506738, lng: 9.190766 };
+    this.position = this.center;
+    this.label = "ciao";
+    this.label2 = "Procione invisibile";
+
+    this.position2 = { lat: 45.506738, lng: 9.191900 };
+    this.positionProcione1 = { lat: 45.506000, lng: 9.190500 };
+    this.positionProcione2 = { lat: 45.506500, lng: 9.191500 };
+    this.positionProcione3 = { lat: 45.506800, lng: 9.193000 };
+    
+    this.circleOptions = { fillColor: 'red' };
+    this.verticesOptions = { fillColor: 'red' };
+    this.verticesOptions2 = { fillColor: 'red' };
+    
+    this.vertices = [
+      { lat: this.center.lat + 0.001, lng: this.center.lng - 0.002 },
+      { lat: this.center.lat, lng: this.center.lng },
+      { lat: this.center.lat - 0.001, lng: this.center.lng - 0.002 }
+    ];
+    this.vertices2 = [
+      { lat: this.center.lat + 0.001, lng: this.center.lng + 0.002},
+      { lat: this.center.lat + 0.001, lng: this.center.lng },
+      { lat: this.center.lat - 0.001, lng: this.center.lng },
+      { lat: this.center.lat - 0.001, lng: this.center.lng + 0.002 }
+    ];
+
+    let iconData: google.maps.Icon = {
+      url: './assets/img/cat_acrobat.ico',
+      scaledSize: new google.maps.Size(60, 60)
+    }
+    
+    let iconData2: google.maps.Icon = {
+      url: './assets/img/raccoon (1).png',
+      scaledSize: new google.maps.Size(60, 60)
+    }
+    let iconData3: google.maps.Icon = {
+      url: './assets/img/raccoon (2).png',
+      scaledSize: new google.maps.Size(60, 60)
+    }
+    let iconData4: google.maps.Icon = {
+      url: './assets/img/raccoon (3).png',
+      scaledSize: new google.maps.Size(60, 60)
+    }
+    this.markerOptions = { icon: iconData }
+    this.markerOptions1 = { icon: iconData2 }
+    this.markerOptions2 = { icon: iconData3 }
+    this.markerOptions3 = { icon: iconData4 }
+  }
+
+  redChange() {
+    this.circleOptions = { fillColor: 'red' }
+    this.verticesOptions = { fillColor: 'red' };
+  }
+  pinkChange() {
+    this.circleOptions = { fillColor: 'pink' }
+    this.verticesOptions = { fillColor: 'pink' };
+  }
+  purpleChange() {
+    this.circleOptions = { fillColor: 'purple' }
+    this.verticesOptions = { fillColor: 'purple' };
+  }
+  redChangeR() {
+    this.verticesOptions2 = { fillColor: 'red' }
+  }
+  pinkChangeR() {
+
+    this.verticesOptions2 = { fillColor: 'pink' }
+  }
+  purpleChangeR() {
+
+    this.verticesOptions2 = { fillColor: 'purple' }
+  }
+
+  
+}
+
